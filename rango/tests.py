35ab8f8c2ef5bb38b4660dc3f0484ad728529a46
vzbiven <vzbiven@yandex.ru>
2019-04-14 13:49:58 +0300

    added gitignore

--- rango/tests.py
+++ rango/tests.py
@@ -17,25 +17,25 @@ class IndexPageTests(TestCase):
     def test_index_contains_hello_message(self):
         # Check if there is the message 'Rango Says'
         # Chapter 4
-        response = self.client.get(reverse('index'))
+        response = self.client.get(reverse('rango:index'))
         self.assertIn(b'Rango says', response.content)
 
     def test_index_using_template(self):
         # Check the template used to render index page
         # Chapter 4
-        response = self.client.get(reverse('index'))
+        response = self.client.get(reverse('rango:index'))
         self.assertTemplateUsed(response, 'rango/index.html')
 
     def test_rango_picture_displayed(self):
         # Check if is there an image called 'rango.jpg' on the index page
         # Chapter 4
-        response = self.client.get(reverse('index'))
+        response = self.client.get(reverse('rango:index'))
         self.assertIn(b'rango.jpg', response.content)
 
     def test_index_has_title(self):
         # Check to make sure that the title tag has been used
         # And that the template contains the HTML from Chapter 4
-        response = self.client.get(reverse('index'))
+        response = self.client.get(reverse('rango:index'))
         self.assertIn(b'<title>', response.content)
         self.assertIn(b'</title>', response.content)
 
@@ -45,20 +45,20 @@ class AboutPageTests(TestCase):
     def test_about_contains_create_message(self):
         # Check if in the about page is there - and contains the specified message
         # Exercise from Chapter 4
-        response = self.client.get(reverse('about'))
+        response = self.client.get(reverse('rango:about'))
         self.assertIn(b'This tutorial has been put together by', response.content)
 
 
     def test_about_contain_image(self):
         # Check if is there an image on the about page
         # Chapter 4
-        response = self.client.get(reverse('about'))
+        response = self.client.get(reverse('rango:about'))
         self.assertIn(b'img src="/media/', response.content)
 
     def test_about_using_template(self):
         # Check the template used to render index page
         # Exercise from Chapter 4
-        response = self.client.get(reverse('about'))
+        response = self.client.get(reverse('rango:about'))
 
         self.assertTemplateUsed(response, 'rango/about.html')
 
@@ -68,28 +68,28 @@ class AboutPageTests(TestCase):
 class Chapter4ViewTests(TestCase):
     def test_index_contains_hello_message(self):
         # Check if there is the message 'hello world!'
-        response = self.client.get(reverse('index'))
+        response = self.client.get(reverse('rango:index'))
         self.assertIn( b'Rango says', response.content)
 
     def test_does_index_contain_img(self):
         # Check if the index page contains an img
-        response = self.client.get(reverse('index'))
+        response = self.client.get(reverse('rango:index'))
         self.assertIn(b'img',response.content)
 
     def test_about_using_template(self):
         # Check the template used to render index page
         # Exercise from Chapter 4
-        response = self.client.get(reverse('about'))
+        response = self.client.get(reverse('rango:about'))
         self.assertTemplateUsed(response, 'rango/about.html')
 
     def test_does_about_contain_img(self):
         # Check if in the about page contains an image
-        response = self.client.get(reverse('about'))
+        response = self.client.get(reverse('rango:about'))
         self.assertIn(b'img',response.content)
 
     def test_about_contains_create_message(self):
         # Check if in the about page contains the message from the exercise
-        response = self.client.get(reverse('about'))
+        response = self.client.get(reverse('rango:about'))
         self.assertIn(b'This tutorial has been put together by',response.content)
 
 
