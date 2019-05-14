53b24962604bcf618879ac9b5ddb83962820ebe9
vzbiven <vzbiven@yandex.ru>
2019-05-14 11:11:26 +0300

    list of categories on main page

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 
+from rango.models import Category
+
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake'}
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by the number of likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary (with our boldmessage!) 
+    # that will be passed to the template engine
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake'
+    context_dict['categories'] = category_list
+
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
